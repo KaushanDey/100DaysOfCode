a34a2213738d47e53f53c13eb60e1fcae2c46bdd
KaushanDey <kaushandey2003@yahoo.com>
2024-03-15 23:55:25 +0530

    Day 72 commit

---
+++ Day 72/G_Orray.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cin.exceptions(ios::badbit | ios::failbit);
+#define all(v) v.begin(),v.end()
+#define revall(v) v.rbegin(),v.rend()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define pb push_back
+#define eb emplace_back
+#define mk make_pair
+#define F first
+#define S second
+#define pi acos(-1.0)
+#define maxv(v) *max_element(all(v))
+#define minv(v) *min_element(all(v))
+#define countv(v,a) count(all(v),a)
+#define cntone(x) __builtin_popcountll(x) // number of ones
+#define cntzero(x) __builtin_ctzll(x) // number of zeros
+#define rep(i, l, r) for(long long i = l; i<r; i++)
+#define SUM accumulate
+#define toint(a) atoi(a.c_str())
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+#define Unique(c) (c).resize(unique(all(c))-(c).begin())
+#define sp(a) fixed<<setprecision(a)
+#define printv(container) for(auto it:container) cout << it << " ";cout << endl
+#define vll vector <ll>
+#define pll pair<ll,ll>
+#define mll map<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define vvll vector<vector<ll>>
+#define vvch vector<vector<char>>
+#define cases(i) cout << "Case " << i << ":"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define rand(V) random_shuffle(all(V))
+#define lb(a,X) lower_bound(all(a),X)-a.begin()
+#define ub(a,X) upper_bound(all(a),X)-a.begin()
+#define Tc ll t=1;cin >> t;while(t--)
+#define for0(i,l,r) for(ll i = l;i<r;i++)
+#define for1(i,l,r) for(ll i = l;i<=r;i++)
+#define rev0(i,r,l) for(ll i = r - 1;i>=l;i--)
+vector<ll> a;
+ll N = 100000;
+vector<int> spf(N + 1, 0);
+void smallestPrimeFactor()
+{
+    for (int i = 2; i <= N; i++)
+    {
+        if (!spf[i])
+        {
+            spf[i] = i;
+            for (int j = i * i; j <= N; j += i)
+            {
+                if (!spf[j])
+                    spf[j] = i;
+            }
+        }
+    }
+}
+
+bool Compare(pair<ll,ll> x, pair<ll,ll> y){
+
+    if(x.first!=y.first){
+        if(x.first>y.first) return true;
+        return false;
+    }else{
+        if(a[x.second]>a[y.second]) return true;
+        return false;
+    }
+}
+int main()
+{
+    fastio;
+    Tc
+    {
+        ll n;
+        cin>>n;
+        a.resize(n);
+        vector<bool> isTaken(n,false);
+
+        for(ll i=0;i<n;i++){
+            cin>>a[i];
+        }
+
+        ll curr = 0;
+        ll t = 32;
+        for(ll i=0;i<min(t,n);i++){
+            ll maxi=0,idx;
+            for(ll j=0;j<n;j++){
+                if(!isTaken[j]){
+                    if(maxi<(curr | a[j])){
+                        maxi = curr | a[j];
+                        idx = j;
+                    }
+                }
+            }
+            if(curr==maxi) continue;
+            curr = maxi;
+            isTaken[idx] = 1;
+            cout<<a[idx]<<" ";
+        }
+
+        for(ll i=0;i<n;i++){
+            if(!isTaken[i]) cout<<a[i]<<" ";
+        }
+        cout<<"\n";
+    }
+        
+}
